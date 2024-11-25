1cc19279ff976e91124cc726a999b8ae3d96b32f
sam-fraiday <sam.joel@fraiday.ai>
2024-11-26 00:01:19 +0530

    Include routers in fastapi App

--- src/backend/app/api/v1/router.py
+++ src/backend/app/api/v1/router.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
 from .ga_sql import router as ga_sql_router
+from .endpoints.chat_message import router as chat_message_router
+from .endpoints.chat_session import router as chat_session_router
 
 api_v1_router = APIRouter(prefix="/api/v1")
 
+api_v1_router.include_router(chat_message_router)
+api_v1_router.include_router(chat_session_router)
 api_v1_router.include_router(ga_sql_router)
