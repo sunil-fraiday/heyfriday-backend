c8e0129daa7ae4b197e6b75473567e6eb8bfadd9
sam-fraiday <sam.joel@fraiday.ai>
2024-12-06 15:38:16 +0530

    Add client and client channel related models

--- src/backend/app/models/mongodb/client.py
+++ src/backend/app/models/mongodb/client.py
@@ -5,8 +5,10 @@ from .base import BaseDocument
 class Client(BaseDocument):
     name = fields.StringField(required=True)
     email = fields.EmailField(required=False, default=None)
+    client_id = fields.StringField(required=True, unique=True)
+    client_key = fields.StringField(required=True, unique=True)
     keycloak_config = fields.EmbeddedDocumentField(required=False, default=None)
 
-    active = fields.BooleanField(default=True)
+    is_active = fields.BooleanField(default=True)
 
-    meta = {"collection": "clients", "indexes": ["created_at", "updated_at"]}
+    meta = {"collection": "clients", "indexes": ["created_at", "updated_at", "client_id"]}
